6107f45bd1d3151b0761f2e8c3776bbf55f85fe6
SahaidakYurii <sahaidak.pn@ucu.edu.ua>
2023-05-15 17:46:38 +0300

    made a suffix tree class

--- suf_trie.py
+++ suf_trie.py
@@ -6,7 +6,7 @@ class Node:
     '''
     Creates a class that represents the trie node.
     '''
-    def __init__(self, value):
+    def __init__(self, value = ''):
         '''
         Initializes the node.
         '''
@@ -15,8 +15,11 @@ class Node:
         # Parameter to indicate if the node is a leaf:
         self.is_leaf = False
 
-        # List of child nodes:
-        self.child = []
+        # List of child nodes {value: Node}
+        self.child = {}
+
+    def __repr__(self) -> str:
+        return f"{self.value} >> {str(self.child.keys())[11:-2]}"
 
 
 class SufTrie:
@@ -29,21 +32,49 @@ class SufTrie:
         '''
         # The root of the tree is always an empty string:
         self.root = Node('')
+        self.root.is_leaf = True
 
-    def insert_word(self):
+    def insert_word(self, word: str) -> None:
         '''
         Inserts word into a trie.
         '''
-        pass
+        suffixes = SufTrie.get_suffixes(word)
+        for suf in suffixes:
+            node = self.root
+            for i, ch in enumerate(suf):
+                if ch not in node.child:
+                    if node.is_leaf:
+                        node.is_leaf = False
+                        node.child[''] = Node()
+                        node.child[''].is_leaf = False
 
-    def delete_word(self):
-        '''
-        Deletes the word from the trie.
-        '''
-        pass
+                    node.child[ch] = Node(ch)
+                    if i == len(suf) - 1:
+                        node.child[ch].child[''] = Node()
+                        node.child[ch].child[''].is_leaf = True
 
-    def find_words(self, prefix):
+                node = node.child[ch]
+
+    @classmethod
+    def draw(cls, start_node, depth = 0):
         '''
-        Finds all the words that starts with prefix.
+        draws a tree
         '''
-        pass
+        if isinstance(start_node, SufTrie):
+            start_node = start_node.root
+
+        string = ""
+        tab = "│  "
+
+        for key, node in start_node.child.items():
+            line = tab * depth + key + "\n"
+            string += line + SufTrie.draw(node, depth+1)
+
+        return string
+
+    @classmethod
+    def get_suffixes(cls, word: str) -> list[str]:
+        """
+        returns list of suffixes of a word
+        """
+        return [word[begin:] for begin in range(len(word))]
