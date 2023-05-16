d19cec8329b369c812885b33f89978a229a225f3
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-05-16 17:07:41 +0300

    decomposed classes

--- trie.py
+++ trie.py
@@ -1,7 +1,9 @@
 '''
-Main module to create prefix trie.
+Main module.
 '''
 from typing import Union
+# import networkx as nx
+# import matplotlib.pyplot as plt
 
 class Node:
     '''
@@ -34,17 +36,49 @@ class Node:
         return f"Node({self.value}). Is word {self.is_word}. Children: {list(self.children.keys())}"
 
 
-class Trie:
+class Tree:
     '''
-    Class to create a trie (prefix tree).
+    General class to create prefix/suffix tree.
     '''
     def __init__(self):
         '''
-        Initializes the data.
+        Initializes the tree. The root is always a node with an empty string.
         '''
-        # The root of the tree is always an empty string:
         self.root = Node("", None)
 
+        # Creating a graph to visualize a tree:
+        # self.graph = nx.Graph()
+
+
+    # def draw_graph(self):
+    #     '''
+    #     Visualizes the graph.
+    #     '''
+    #     plt.figure(figsize=(10, 6))
+
+    #     pos = nx.arf_layout(graph)
+    #     nx.draw(graph, pos, node_color='lightblue',
+    #             with_labels=True,
+    #             node_size=500,
+    #             arrowsize=20,
+    #             arrows=True)
+    #     labels = nx.get_edge_attributes(graph, 'weight')
+    #     nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+    #     plt.show()
+
+    # def create_graph(self, tree, index = 0):
+    #     '''
+    #     Creates a graph from tree.
+    #     '''
+    #     if tree.parent:
+    #         self.graph.add_node(tree.value + str(index))
+    #         self.graph.add_edge(tree.value + str(index), tree.parent.value + str(index - 1))
+
+    #     for child in tree.children.values():
+    #         self.create_graph(child, index + 1)
+
+class PrefixTree(Tree):
+    ''' Prefix Tree '''
     def build_tree(self, path: str):
         '''
         Build tree by reading english dict file
@@ -54,6 +88,23 @@ class Trie:
 
         for word in words:
             self.insert_word(word.strip())
+
+        self.compress_tree()
+
+    def compress_tree(self, root: Union[Node, None] = None):
+        ''' ArithmeticError '''
+        node = root or self.root
+
+        if len(node.children) == 1 and not node.is_word:
+            child = node.children[list(node.children.keys())[0]]
+            node.value += child.value
+            node.children = child.children
+            node.is_word = child.is_word
+
+            self.compress_tree(child)
+        else:
+            for child in node.children.values():
+                self.compress_tree(child)
 
     def insert_word(self, word: str):
         '''
@@ -70,7 +121,7 @@ class Trie:
 
         node.is_word = True
 
-    def autocomplete(self, prefix: str, root: Union["Node", None] = None) -> list[str]:
+    def autocomplete(self, prefix: str, root: Union[Node, None] = None) -> list[str]:
         '''
         Finds all the words that starts with prefix.
         '''
@@ -103,35 +154,8 @@ class Trie:
 
         return words
 
+if __name__ == "__main__":
+    tree = PrefixTree()
+    tree.build_tree('words.txt')
 
-trie = Trie()
-trie.build_tree('words.txt')
-
-print(trie.autocomplete('banana'))
-
-# def draw_graph(graph):
-#     plt.figure(figsize=(10, 6))
-
-#     pos = nx.arf_layout(graph)
-#     nx.draw(graph, pos, node_color='lightblue',
-#             with_labels=True,
-#             node_size=500,
-#             arrowsize=20,
-#             arrows=True)
-#     labels = nx.get_edge_attributes(graph, 'weight')
-#     nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
-#     plt.show()
-
-
-# graph = nx.Graph()
-# def create_graph(tree, index = 0):
-#     if tree.parent:
-#         graph.add_node(tree.value + str(index))
-#         graph.add_edge(tree.value + str(index), tree.parent.value + str(index - 1))
-
-#     for child in tree.children.values():
-#         create_graph(child, index + 1)
-
-# create_graph(trie.root)
-# draw_graph(graph)
-# print(trie.root.children)
+    print( tree.autocomplete('banana') )
